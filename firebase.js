361afc57ebac067b41577b0342a9b8e57a95e57e
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-07-02 22:35:33 +0530

    Project intialised with firebase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAIjHXz4oRJ2kRulEWzO0KQnC0vK72olLI",
+    authDomain: "signal-clone-yt-build-1cc00.firebaseapp.com",
+    projectId: "signal-clone-yt-build-1cc00",
+    storageBucket: "signal-clone-yt-build-1cc00.appspot.com",
+    messagingSenderId: "965052947129",
+    appId: "1:965052947129:web:c0cc3161acbb3d21089968"
+  };
