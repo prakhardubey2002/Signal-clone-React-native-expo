313259fa138ef4bad489dd393f5da4542ed4891a
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-07-03 18:10:53 +0530

    react native element 3.4.2 added

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, StackView } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 
 const Stack = createStackNavigator();//React native router functionality is in StackNavigation
-
+const globalScreenOptions={//css component
+  headerStyle:{backgroundColor:'#2C68ED'},
+  headerTitleStyle:{color:'white'},
+  headerTintColor:"white",
+}
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name='Login' component={LoginScreen}/>{/* Path for different Screen */}
+      <Stack.Navigator screenOptions={globalScreenOptions} >
+      <Stack.Screen options={{
+        title:"Lets Sign Up"
+      }} name='Login' component={LoginScreen}/>{/* Path for different Screen */}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,View, Text } from 'react-native'
 import React from 'react'
-
+import { Button,Input,Image } from 'react-native-elements';
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>I am LoginScreen</Text>
+    <View style={styles.container}>
+        <StatusBar style="light" />
+        <Image  style={styles.tinyLogo} source={{
+            uri:"https://images.hindustantimes.com/tech/img/2021/05/12/960x540/signal_app_1610868805441_1610868819838_1620837476101.png",
+        }} />
+      {/* <Text>I am LoginScreen</Text> */}
     </View>
   )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    tinyLogo: {
+        width: 100,
+        height: 100,
+        borderRadius:'2rem',
+      },
+  });
+  
 
 export default LoginScreen
