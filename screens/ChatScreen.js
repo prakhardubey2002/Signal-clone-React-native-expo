c9a4b29b09ea8aa66b0e72fe1644c37b91d75391
Prakhar <prakhardubey2002@gmail.com>
2022-07-16 22:06:56 +0530

    message view added in chat screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,8 +1,10 @@
 import React, { useLayoutEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Avatar} from 'react-native-elements';
-import {AntDesign,FontAwesome,Ionicons} from '@expo/vector-icons';
-
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import { KeyboardAvoidingView, TextInput } from 'react-native-web';
 const ChatScreen = ({ navigation, route }) => {
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -20,22 +22,60 @@ const ChatScreen = ({ navigation, route }) => {
               uri: "https://i.etsystatic.com/27363395/c/1242/987/0/153/il/ffef3d/2896449986/il_340x270.2896449986_13ae.jpg",
             }}
           />
-          <Text style={{color:"white",marginLeft:10,fontWeight:"700"}} >{route.params.chatName}</Text>
-        </View> 
+          <Text style={{ color: "white", marginLeft: 10, fontWeight: "700" }} >{route.params.chatName}</Text>
+        </View>
       ),
       headerLeft: () => (
-       <TouchableOpacity style={{marginLeft:10}}
-       onPress={navigation.goBack}
-       >
-        <AntDesign name='arrowleft' size={24} color="white" />
-       </TouchableOpacity>
+        <TouchableOpacity style={{ marginLeft: 10 }}
+          onPress={navigation.goBack}
+        >
+          <AntDesign name='arrowleft' size={24} color="white" />
+        </TouchableOpacity>
       ),
+      headerRight: () => (
+        <View style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: 80,
+          marginRight: 20,
+        }} >
+          <TouchableOpacity>
+            <FontAwesome name='video-camera' size={24} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='call' size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      )
     });
   }, [navigation])
   return (
-    <View>
-      <Text>{route.params.chatName}</Text>
-    </View>
+    <SafeAreaView>
+    <StatusBar style='light' >
+      <KeyboardAvoidingView
+        behavior={Platform === 'ios' ? "padding" : "height"}
+        style={styles.container}
+        keyboardVertivalOffset={90}
+      >
+        <>
+          <ScrollView>
+
+          </ScrollView>
+          <View style={styles.footer}>
+            <TextInput
+              value={input}
+              onChangeText={(text) => setInput(text)}
+              placeholder="Signal Message"
+              style={styles.textInput} />
+            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5} >
+              <Ionicons name='send' size={24} color="#2B68E6" />
+            </TouchableOpacity>
+          </View>
+        </>
+
+      </KeyboardAvoidingView>
+    </StatusBar>
+  </SafeAreaView>
   )
 }
 
