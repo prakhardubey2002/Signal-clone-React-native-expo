6d90e5756962dcbdd172ad188fae3213fd5a66c5
Prakhar <prakhardubey2002@gmail.com>
2022-07-17 16:49:16 +0530

    Chat screen UI fixed

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,12 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, TextInput } from 'react-native-web';
+import { KeyboardAvoidingView, ScrollView, TextInput } from 'react-native-web';
 const ChatScreen = ({ navigation, route }) => {
+  const [input, setInput] = useState();
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Chat",
@@ -49,38 +50,68 @@ const ChatScreen = ({ navigation, route }) => {
       )
     });
   }, [navigation])
+  const sendMessage = () => {
+
+  }
   return (
-    <SafeAreaView>
-    <StatusBar style='light' >
+    <SafeAreaView style={{flex:1,backgroundColor:"white"}} >
+      <StatusBar style='light' />
       <KeyboardAvoidingView
-        behavior={Platform === 'ios' ? "padding" : "height"}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.container}
-        keyboardVertivalOffset={90}
+        keyboardVerticalOffset={90}
       >
         <>
           <ScrollView>
-
+            
           </ScrollView>
+
           <View style={styles.footer}>
+
             <TextInput
               value={input}
               onChangeText={(text) => setInput(text)}
               placeholder="Signal Message"
-              style={styles.textInput} />
+              style={styles.textInput} 
+              />
+
             <TouchableOpacity onPress={sendMessage} activeOpacity={0.5} >
+
               <Ionicons name='send' size={24} color="#2B68E6" />
+
             </TouchableOpacity>
+
           </View>
         </>
 
       </KeyboardAvoidingView>
-    </StatusBar>
-  </SafeAreaView>
+
+    </SafeAreaView>
   )
 }
 
 export default ChatScreen
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+  },
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    padding: 15,
+  },
+  textInput: {
+    bottom: 0,
+    height: 40,
+    flex: 1,
+    marginRight: 15,
+    borderColor: "transperant",
+    backgroundColor: "#ECECEC",
+    borderWidth: 1,
+    padding: 10,
+    color: "grey",
+    borderRadius: 30,
+  },
 })
